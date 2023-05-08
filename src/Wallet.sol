86d6b5b5685c6f1f7dfe7b9b2f03bb3ca7be18b0
t4sk <tsk.nakamura@gmail.com>
2023-05-08 18:18:47 +0900

    wallet

--- src/Wallet.sol
+++ src/Wallet.sol
@@ -3,11 +3,15 @@ pragma solidity ^0.8.18;
 contract Wallet {
     address payable public owner;
 
+    event Deposit(address account, uint amount);
+
     constructor() payable {
         owner = payable(msg.sender);
     }
 
-    receive() external payable {}
+    receive() external payable {
+        emit Deposit(msg.sender, msg.value);
+    }
 
     function withdraw(uint256 _amount) external {
         require(msg.sender == owner, "caller is not owner");
