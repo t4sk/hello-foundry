48bb29d4b96fa8ac2e034cc744826ecd15ecff76
t4sk <tsk.nakamura@gmail.com>
2023-06-19 18:39:00 +0900

    diff test

--- exp.py
+++ exp.py
@@ -2,12 +2,19 @@ import sys
 import math
 from eth_abi import encode
 
+# python exp.py 18446744073709551616
+# 2.718...
+
 x = float(sys.argv[1]) / 2**64
 y = math.exp(x)
 
+# Debug
+# print(y)
+
+# Check y < 2**64
 assert y < 2**64, f"x = {x}"
 y = int(y * 2**64)
 
+# Encode
 y = "0x" + encode(["int128"], [y]).hex()
 print(y)
-
