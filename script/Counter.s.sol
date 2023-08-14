29fe366ad547556cc3151f2d4e3a55c5c9af2835
t4sk <tsk.nakamura@gmail.com>
2023-08-14 13:43:26 +0900

    deploy

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -2,11 +2,27 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
+import {Counter} from "../src/Counter.sol";
+
+/*
+source .env
+forge script script/Counter.s.sol:CounterScript --rpc-url $GOERLI_RPC_URL --broadcast --verify -vvvv
+*/
 
 contract CounterScript is Script {
     function setUp() public {}
 
     function run() public {
-        vm.broadcast();
+        uint256 privateKey = vm.envUint("DEV_PRIVATE_KEY");
+
+        vm.startBroadcast(privateKey);
+
+        Counter counter = new Counter();
+
+        counter.inc();
+        counter.inc();
+        counter.inc();
+
+        vm.stopBroadcast();
     }
 }
